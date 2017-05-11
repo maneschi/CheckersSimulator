9ed9cdb8d558fd57f97865589c5671e0f2ff2ac5
maneschi <richard.john.maneschi@gmail.com>
2017-05-11 16:15:36 +1000

    Uploading CheckersSimulator

---
+++ Assets/PiecePicker.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GGL;                     // for Manny's GizmoGL
+public class PiecePicker : MonoBehaviour {
+    public float pieceHeight = 5f;
+    public float rayDistance = 1000f;
+    public LayerMask selectionIgnoreLayer;
+    private Piece selectedPiece;
+    private Checkerboard board;
+	// Use this for initialization
+	void Start () {
+        board = FindObjectOfType<Checkerboard>();
+        if (board == null) Debug.LogError("no board");
+	}
+	void CheckSelection()
+    {
+        //perform a ray cast from camera mouse position to world
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        GizmosGL.color = Color.red;
+        GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance, 0.1f, 0.3f);
+        RaycastHit hit;
+        //check mouse buttton
+        if (Input.GetMouseButtonDown (0))
+        {
+            //cast ray to detect piece
+            if(Physics.Raycast (ray, out hit, rayDistance ))
+            {
+                //Set the selected piece to be hit object
+                selectedPiece = hit.collider.GetComponent<Piece>();
+                if (selectedPiece == null) Debug.Log("Cannot pick up object:" + hit.collider.name);
+            }
+        }
+
+    }
+    void MoveSelection()
+    {
+        //chec if piece selected
+        if (selectedPiece != null)
+        {
+            //make ray from camera
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            GizmosGL.color = Color.yellow;
+            GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance, 0.1f, 0.3f);
+
+            RaycastHit hit;
+
+            if (Physics .Raycast (ray,out hit,rayDistance, ~selectionIgnoreLayer ))    //tilde  bitwise
+            {
+                //Obtain the hit point
+                GizmosGL.color = Color.blue;
+                GizmosGL.AddSphere(hit.point, 0.5f);
+                Vector3 piecePos = hit.point + Vector3.up * pieceHeight;
+                selectedPiece.transform.position = piecePos;
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        CheckSelection();
+        MoveSelection();
+       // print(Input.mousePosition);
+	}
+}
---
+++ Assets/Scripts/Checkerboard.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkerboard : MonoBehaviour {
+    public GameObject blackPiece, whitePiece;
+    public int boardX = 8, boardZ = 8;
+    public float pieceRadius = 0.5f;
+    public Piece[,] pieces;
+    private int halfBoardX, halfBoardZ;
+    private float pieceDiameter;
+    private Vector3 bottomLeft;
+
+	// Use this for initialization
+	void Start () {
+        halfBoardX = boardX / 2;
+        halfBoardZ = boardZ / 2;
+        pieceDiameter = pieceRadius * 2f;
+        bottomLeft = transform.position - Vector3.right * halfBoardX - Vector3.forward * halfBoardZ;
+        CreateGrid();
+	}
+
+    void CreateGrid()
+    {
+        //init 2d array
+        pieces = new Piece[boardX, boardZ];
+        #region Generate White Pieces
+        // loop thru
+        for(int x=0; x < boardX; x += 2)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                bool evenRow = z % 2 == 0;               //modulus 2
+                int gridX = evenRow ? x : x + 1;   //ternary
+                int gridZ = z;
+
+                //gen piece
+                GeneratePiece(whitePiece, gridX, gridZ);
+            }
+        }
+
+        #endregion
+        #region Generate Black Pieces
+        for (int x = 0; x < boardX; x += 2)
+        {
+            for (int z = boardZ-3; z < boardZ; z++)
+            {
+                bool evenRow = z % 2 == 0;               //modulus 2
+                int gridX = evenRow ? x : x + 1;   //ternary
+                int gridZ = z;
+
+                //gen piece
+                GeneratePiece(blackPiece, gridX, gridZ);
+            }
+        }
+        #endregion
+    }
+
+    void GeneratePiece(GameObject piecePrefab, int x, int z)                             //GO tab > GameObject
+    {
+        GameObject clone = Instantiate(piecePrefab);
+        clone.transform.SetParent(transform);
+        Piece piece = clone.GetComponent<Piece>();
+        PlacePiece(piece, x, z);
+
+    }
+
+    void PlacePiece(Piece piece,int x, int z)
+    {
+        //calc offset
+        float xOffset = x * pieceDiameter + pieceRadius;
+        float zOffset = z * pieceDiameter + pieceRadius;
+
+        //Set pieces new grid coordinates
+        piece.gridX = x;
+        piece.gridZ = z;
+
+        //Move piece to board coordinate
+        piece.transform.position = bottomLeft + Vector3.right * xOffset + Vector3.forward * zOffset;
+
+        //Set piece in array slot
+        pieces[x, z] = piece;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
